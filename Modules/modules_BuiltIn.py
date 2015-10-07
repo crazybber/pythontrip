b2661f4809ccf2d955653c38264341fff14a9c45
Eamon <chenlulei@live.com>
2015-10-07 21:34:30 +0800

    add codes

--- Modules/modules_BuiltIn.py
+++ Modules/modules_BuiltIn.py
@@ -42,3 +42,52 @@ print(now)
 # datetime.datetime(2015,10,05,20,12,58,10054)
 
 print(now+timedelta(hours=10))
+
+
+from datetime import timezone
+
+tz_utc_8 = timezone(timedelta(hours=8))
+
+now= datetime.now()
+
+print(now)
+
+dt=now.replace(tzinfo=tz_utc_8)
+
+print(dt)
+
+print('------------------------')
+utc_dt=datetime.utcnow().replace(tzinfo=timezone.utc)
+
+print(utc_dt)
+
+bjtm=utc_dt.astimezone(timezone(timedelta(hours=8)))
+print(bjtm)
+
+tokyo_tm=bjtm.astimezone(timezone(timedelta(hours=9)))
+print('------------------------')
+print(tokyo_tm)
+
+
+
+import re
+
+
+def to_timestamp(dt_str,tz_str):
+	tz_fmt_str='^UTC([+-]\d{1,2})\:\d{2}$'
+	tm_fmt=re.match(tz_fmt_str,tz_str)
+	if tm_fmt:
+		tz_hours=int(tm_fmt.group(1))
+		cur_datetime=datetime.strptime(dt_str,'%Y-%m-%d %H:%M:%S')
+		return cur_datetime.replace(tzinfo=timezone(timedelta(hours=tz_hours))).timestamp()
+		
+		
+
+		
+t1 = to_timestamp('2015-6-1 08:10:30', 'UTC+7:00')
+assert t1 == 1433121030.0, t1
+
+print('Pass')
+	
+	
+	
