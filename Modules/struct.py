e3440880346c3891bd61a1b7575a772c54027f16
edward <eamon_chen@outlook.com>
2018-09-01 18:58:46 +0800

    fix 2357 update feature for code print

--- Modules/struct.py
+++ Modules/struct.py
@@ -10,7 +10,6 @@ __Author__ ='eamon'
 # b2 = n & 0xff0000 >> 16
 # b3 = n & 0xff00 >> 8
 # b4 = n & 0xff
-
 # b5= bytes([b1,b2,b3,b4])
 
 # print(b5)
@@ -20,6 +19,6 @@ import struct
 r=struct.pack('>I',10240099)
 print(r)
 
-# r=struct.unpack('>IH', b'\xf0\xf0\xf0\xf0\x80\x80')
+r=struct.unpack('>IH', b'\xf0\xf0\xf0\xf0\x80\x80')
 
-# print(r)
+print(r)
